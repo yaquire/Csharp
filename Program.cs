384fc0c6389a7c819a5a3782a63c0d7ee570f727
yaq <yaq147@gmail.com>
2024-08-29 20:18:59 +0800

    trying to do ecc for inputing for a binary

--- Program.cs
+++ Program.cs
@@ -22,13 +22,34 @@ string input_choice_function()//This function lets the user choses which way to 
   }
 }
 
+string binary_decimal_function()
+{
+  int number;
+  Console.Write("Please Enter the Binary Number: ");
+  while (!int.TryParse(Console.ReadLine(), out number))
+  {
+    Console.Write("Invalid Input. PLease Enter a proper Binary Number: ");
+  }
+  //Console.Write(number);
+  bool has_binary = false;
+  for (int i = 0; i < 10; i++)
+  {
+
+  }
+
+  return "b-d";
+}
 
 
 
 
 void main()
 {
-  input_choice_function();
+  string user_choice = input_choice_function();
+  if (user_choice == "binary-decimal")
+  {
+    binary_decimal_function();
+  }
 }
 
 main();
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d716e9cc84a6da9412e4f2ed4ac30545fba7ddc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
